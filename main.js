b11365c4df9f48b48e0a76f83a76985926493b92
kumardevanshuu <144575092+kumardevanshuu@users.noreply.github.com>
2023-10-31 13:58:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings: ["Student" ,"Java Developer","Python Developer","Web Developer", "Bharat Intern"],
+    typespeed:100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+});
